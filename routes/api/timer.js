302c31653757a79e1c5752d166132e9f17a1b6bb
heintzz <mhasnanr@gmail.com>
2023-03-24 14:31:23 +0700

    feat: relocate the verifyJWT middleware

--- routes/api/timer.js
+++ routes/api/timer.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const timerController = require('../../controller/timerController')
+const verifyJWT = require('../../middleware/verifyJWT')
 
 router
-    .route('/')
-    .get(timerController.getTimer)
-    .post(timerController.updateTimer)
+  .route('/')
+  .get(verifyJWT, timerController.getTimer)
+  .post(verifyJWT, timerController.updateTimer)
 
 module.exports = router
